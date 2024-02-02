2693dd111ab2c5c4f2873e0fdb314443719e805e
Raghav Kattel <arg.ahv7@gmail.com>
2024-02-02 15:25:31 +0545

    Add path module and update project creation logic

--- apps/rahat/src/projects/project.service.ts
+++ apps/rahat/src/projects/project.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { PrismaService } from '@rumsan/prisma';
+import path from 'path';
 import { EVENTS } from '../constants/events';
 import { CreateProjectDto } from './dto/create-project.dto';
 @Injectable()
@@ -23,11 +24,13 @@ export class ProjectService {
     });
 
     //get directory path
-    const path = process.cwd() + '/projects' + '/template';
-    console.log('path', path);
+    const dirPath = path.join(process.cwd() + '\\projects' + '\\templates');
     //+ project.id + '/';
 
-    this.eventEmitter.emit(EVENTS.PROJECT_CREATED, { ...project, path });
+    this.eventEmitter.emit(EVENTS.PROJECT_CREATED, {
+      ...project,
+      path: dirPath,
+    });
     // if (!project) throw new Error('Project not created');
 
     return project;
--- apps/worker/project.bull.worker.ts
+++ apps/worker/project.bull.worker.ts
@@ -1,7 +1,24 @@
-import { Worker } from 'bullmq';
+import { Job, Worker } from 'bullmq';
 import { exec } from 'child_process';
 import fs from 'fs';
 import path from 'path';
+
+type ProjectData = {
+  id: number;
+  uuid: string;
+  name: string;
+  description: string;
+  status: string;
+  projectType: string;
+  extras: string;
+  instance_id: null | string;
+  contract_address: null | string;
+  createdAt: string;
+  updatedAt: string;
+  path: string;
+};
+
+const projectCreateJobName = 'project_create';
 
 const templatePath = (projectType: string) =>
   path.join(process.cwd(), `/apps/rahat/src/projects/templates/${projectType}`);
@@ -10,6 +27,23 @@ const originalTemplateEnv = fs.readFileSync(
   path.join(process.cwd(), '.env'),
   'utf-8'
 );
+
+const updateDockerComposeFile = (
+  filePath: string,
+  projectType: string,
+  projectName: string,
+  uuid: string
+) => {
+  let fileContent = fs.readFileSync(filePath, 'utf8');
+
+  fileContent = fileContent.replace('projectType', projectType);
+  fileContent = fileContent.replace(
+    'projectName',
+    projectName.replace(/ /g, '_')
+  );
+  fileContent = fileContent.replace('uuid', uuid);
+  fs.writeFileSync(filePath, fileContent);
+};
 
 const replaceEnvWithConfig = (original, destination) => {
   const originalEnv = original.split('\n');
@@ -22,7 +56,12 @@ const replaceEnvWithConfig = (original, destination) => {
   return newEnv.join('\n');
 };
 
-const createProjectDirectory = (dirPath, projectType, projectUUID) => {
+const createProjectDirectory = (
+  dirPath,
+  projectType,
+  projectName,
+  projectUUID
+) => {
   const fullPath = path.join(dirPath, projectUUID);
   const templateFile = templatePath(projectType);
   fs.mkdirSync(fullPath, { recursive: true });
@@ -38,14 +77,28 @@ const createProjectDirectory = (dirPath, projectType, projectUUID) => {
     fs.readFileSync(path.join(fullPath, '.env'), 'utf-8'),
     originalTemplateEnv
   );
+
+  updateDockerComposeFile(
+    path.join(fullPath, 'docker-compose.yml'),
+    projectType,
+    projectName,
+    projectUUID
+  );
+
   fs.writeFileSync(path.join(fullPath, '.env'), newEnv);
   return fullPath;
 };
 
-const readAndExecuteEnvTemplateFile = (dirPath, projectType, projectUUID) => {
+const readAndExecuteEnvTemplateFile = (
+  dirPath,
+  projectType,
+  projectName,
+  projectUUID
+) => {
   const newProjectPath = createProjectDirectory(
     dirPath,
     projectType,
+    projectName,
     projectUUID
   );
   runDockerCompose(newProjectPath);
@@ -68,10 +121,15 @@ const runDockerCompose = (dirPath) => {
   );
 };
 
-const jobProcessor = async (job) => {
+const jobProcessor = async (job: Job<ProjectData, ProjectData, string>) => {
+  if (job.name !== projectCreateJobName) return;
+
+  console.log('Project Creation Job Started...=====>');
+
   readAndExecuteEnvTemplateFile(
     job.data.path,
     job.data.projectType,
+    job.data.name,
     job.data.uuid
   );
   return job.data;
@@ -79,13 +137,18 @@ const jobProcessor = async (job) => {
 
 const bullWorker = (queueName, processor, config) => {
   const worker = new Worker(queueName, processor, config);
-  worker.on('completed', (job) => {
-    console.log(`Job completed with result`, job.returnvalue);
+  worker.on('completed', (job: Job<ProjectData, ProjectData, string>) => {
+    console.log(
+      `Project Created, Name: `,
+      job.data.name,
+      'UUID:' + job.data.uuid
+    );
+    // console.log('Project Creation Job Ended...=====>');
   });
   return { worker };
 };
 
-bullWorker('APP_QUEUE', jobProcessor, {
+bullWorker('HOST', jobProcessor, {
   connection: {
     host: 'localhost',
     port: 6379,
